c299ce608563930f472840807f6452858f40c2e2
Ho Mun <mhhh2082@gmail.com>
2024-11-03 23:14:10 +0900

    [FEAT/#11] alertSettingMainView+Component

---
+++ BlueberrySmoothie/BlueBerrySmoothie/Component/ActionButton.swift
@@ -0,0 +1,29 @@
+//
+//  ActionButton.swift
+//  BlueBerrySmoothie
+//
+//  Created by 문호 on 11/1/24.
+//
+
+import SwiftUI
+
+struct ActionButton: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.clear)
+                .background(Color(red: 104.0 / 255.0, green: 144.0 / 255.0, blue: 1.0))
+                .cornerRadius(10.0)
+            
+            Text("시작하기")
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(.white)
+                .padding()
+        }
+        .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+#Preview {
+    ActionButton()
+}
