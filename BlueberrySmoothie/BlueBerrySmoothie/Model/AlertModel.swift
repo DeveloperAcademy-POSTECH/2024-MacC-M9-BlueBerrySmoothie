85a96fb34fc2b6f9b0f914ff942ad9e08f3563c7
yeji <49176310+Yeggggi@users.noreply.github.com>
2024-11-06 01:42:47 +0900

    [FEAT/#37] BusStopAlert에서 불필요한 변수 삭제

--- BlueberrySmoothie/BlueBerrySmoothie/Model/AlertModel.swift
+++ BlueberrySmoothie/BlueBerrySmoothie/Model/AlertModel.swift
@@ -72,14 +72,11 @@ class BusStopLocal {
 struct BusStopAlert: Identifiable {
     var id = UUID()
     var cityCode: Double // 도시코드
-    var bus: Bus // 버스 번호, 노선 id 저장되어있음
+    var bus: Bus // 버스 번호, 노선d id 저장되어있음
+    var allBusStop: [BusStop]
     var arrivalBusStop: BusStop // 도착 정류장
     var alertBusStop: Int // 알람 줄 정류장
-    var alertLabel: String // 알람 이름
     var firstBeforeBusStop: BusStop? // 1번째 전 정류장
     var secondBeforeBusStop: BusStop? // 2번째 전 정류장
     var thirdBeforeBusStop: BusStop? // 3번째 전 정류장
-    var alertSound: UNNotificationSound? // 알람 사운드 (옵셔널)
-    var alertHaptic: UIImpactFeedbackGenerator.FeedbackStyle? //알람 진동 (옵셔널)
-    var alertCycle: Double? //알람 주기 (옵셔널)
 }
