a969dd09271cdfb58cad68655d3c4642214b9228
tenedict <tenedict@gmail.com>
2024-11-24 12:30:57 +0900

    [Feat/#111] 초성인덱스 햅틱 추가

--- BlueberrySmoothie/BlueBerrySmoothie/App/BlueBerrySmoothieApp.swift
+++ BlueberrySmoothie/BlueBerrySmoothie/App/BlueBerrySmoothieApp.swift
@@ -16,14 +16,13 @@ struct BlueBerrySmoothieApp: App {
 
     var body: some Scene {
         WindowGroup {
-//            MainView()
-//                .environmentObject(busStopViewModel)
-//                .modelContainer(for: [BusAlert.self, BusStopLocal.self])
-//          .onAppear {
-//                    // 필요시 추가 초기화 작업
-//                    NotificationManager.instance.requestAuthorization() // 권한 요청 예시
-//                }
-            CitySettingView()
+            MainView()
+                .environmentObject(busStopViewModel)
+                .modelContainer(for: [BusAlert.self, BusStopLocal.self])
+          .onAppear {
+                    // 필요시 추가 초기화 작업
+                    NotificationManager.instance.requestAuthorization() // 권한 요청 예시
+                }
         }
 
     }
