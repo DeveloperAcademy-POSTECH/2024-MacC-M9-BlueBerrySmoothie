8c401a58d1e4d29d826d1ead9c4c86c16ce0af69
tenedict <tenedict@gmail.com>
2024-11-24 18:07:49 +0900

    [Fix] 오류 수정

--- BlueberrySmoothie/BlueBerrySmoothie/OnBoarding&Splash/AppView.swift
+++ BlueberrySmoothie/BlueBerrySmoothie/OnBoarding&Splash/AppView.swift
@@ -12,12 +12,10 @@ struct AppView: View {
     @AppStorage("hasSeenOnboarding") private var hasSeenOnboarding = false
     
     var body: some View {
-        NavigationView {
             if hasSeenOnboarding {
                 MainView()  // 온보딩을 본 후에는 MainView로 이동
             } else {
                 OnboardingView()  // 온보딩을 아직 보지 않았다면 OnboardingView로 이동
             }
-        }
     }
 }
