9a78f71560b10a7dbc7f89f365e6b479781686e1
JinBella <dlwlsrud0622@naver.com>
2024-11-25 12:40:17 +0900

    LiveAct 80% 완료

--- BlueberrySmoothie/LiveActWidget/LiveActivityUI.swift
+++ BlueberrySmoothie/LiveActWidget/LiveActivityUI.swift
@@ -16,44 +16,31 @@ struct LiveActivityUI: Widget {
 
                     Text("핫챠") // 앱 이름
                         .font(.headline) // 헤드라인 글씨체
-//                        .fontWeight(.light) // 얇은 글씨체 설정
                         .foregroundColor(.orange)
-                        .font(.headline)
                         .fontWeight(.bold)
                     
                     Spacer()
                     Text("등교알람") // 앱 이름
-                        .font(.headline) // 헤드라인 글씨체
+                        .font(.subheadline) // 작은 글씨체 설정
                         .fontWeight(.light) // 얇은 글씨체 설정
                         .foregroundColor(.orange)
-                        .font(.headline)
-                        .fontWeight(.bold)
                 }
                 .padding(.bottom, 5)
 
                 Text("알람까지 \(context.state.stopsRemaining) 정거장 남았습니다.") // 남은 정류장
-                    .font(.headline)
+                    .font(.title3) // 글씨 크기를 크게 설정
                     .foregroundColor(.black)
                     .padding(.bottom, 10)
                 
-                Text("현재 정류장은 \(context.state.currentStop)입니다.") // 현재 정류장 정보
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                    .padding(.bottom, 5)
-
-                VStack {
-                    Text(progressMessage(for: context.state.progress)) // 진행 상태 메시지
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .fontWeight(.bold)
-
-                    ProgressView(value: context.state.progress, total: 1.0) // 진행 상태 바
-                        .progressViewStyle(LinearProgressViewStyle())
-                        .frame(height: 10)
-                        .accentColor(.blue)
-                        .padding(.top, 5)
-                }
-                .padding()
+                VStack(alignment: .leading) {
+                                   Text("현재 정류장은")
+                                       .font(.subheadline)
+                                       .foregroundColor(.gray)
+                                   Text("\(context.state.currentStop)입니다.") // 현재 정류장 + "입니다."
+                                       .font(.subheadline)
+                                       .foregroundColor(.orange) // 주황색으로 설정
+                               }
+                               .padding(.bottom, 5)
             }
             .padding()
         } dynamicIsland: { context in
@@ -69,8 +56,8 @@ struct LiveActivityUI: Widget {
                 DynamicIslandExpandedRegion(.trailing) {
                     Text("\(context.state.stopsRemaining) 정거장 전") // 남은 정류장
                         .font(.body)
-                                .fontWeight(.bold)  // 볼드체 설정
-                                .foregroundColor(.white)  // 하얀색 텍스트로 설정
+                        .fontWeight(.bold)  // 볼드체 설정
+                        .foregroundColor(.white)  // 하얀색 텍스트로 설정
                 }
 
                 DynamicIslandExpandedRegion(.center) {
@@ -89,28 +76,11 @@ struct LiveActivityUI: Widget {
             } compactTrailing: {
                 Text("\(context.state.stopsRemaining) 정거장 전")
                     .font(.body)
-                            .fontWeight(.bold)  // 볼드체 설정
-                            .foregroundColor(.white)  // 하얀색 텍스트로 설정
-
+                    .fontWeight(.bold)  // 볼드체 설정
+                    .foregroundColor(.white)  // 하얀색 텍스트로 설정
             } minimal: {
                 Image("AppIcon")
             }
-        }
-    }
-
-    // 진행 상태 메시지 반환
-    func progressMessage(for progress: Double) -> String {
-        switch progress {
-        case 1.0:
-            return "하차 완료"
-        case 0.75..<1.0:
-            return "하차 준비"
-        case 0.5..<0.75:
-            return "목적지 임박"
-        case 0.25..<0.5:
-            return "다음 정류장까지"
-        default:
-            return "진행 중"
         }
     }
 }
